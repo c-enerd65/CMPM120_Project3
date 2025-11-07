82d34981217b6178cafcb6411318254843da0fd8
Nana <cesnard@ucsc.edu>
2025-11-07 00:15:35 -0800

    added_comments

--- asgn3_group/platformer/src/gameObjects/player.js
+++ asgn3_group/platformer/src/gameObjects/player.js
@@ -3,6 +3,7 @@ class Player extends Phaser.GameObjects.Sprite {
     {
         super(scene, x, y, name);
         
+        //general init
         this.name = name;
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -10,18 +11,24 @@ class Player extends Phaser.GameObjects.Sprite {
         this.body.setAllowGravity(true);
         this.body.setCircle(12);
 
+        //Status variables [subject to change]
         this.score = 0;
-        this.health = 750;
+        this.totalLives = 3;
+        this.stamina = 100; 
         
+        //player boost flag
         this.hasBoost = false;
         this.boost = 1;
 
+        //init animations
         this.init();
+        //set player controls
         this.playerControls();
     }
 
     init()
     {
+        //idle animation
         this.scene.anims.create({
             key: 'idle',
             frames: this.anims.generateFrameNumbers(this.name, { start: 0, end: 0 }),
@@ -30,6 +37,7 @@ class Player extends Phaser.GameObjects.Sprite {
             yoyo: true
         });
 
+        //walk animation
         this.scene.anims.create({
             key: 'walk',
             frames: this.anims.generateFrameNumbers(this.name, { start: 0, end: 1 }),
@@ -38,6 +46,7 @@ class Player extends Phaser.GameObjects.Sprite {
             yoyo: true
         });
 
+        //defaults idle
         this.anims.play('idle', true);
     }
 
@@ -45,22 +54,32 @@ class Player extends Phaser.GameObjects.Sprite {
     {
         this.keyIn = this.scene.input.keyboard.createCursorKeys();
 
+        // player jumps when pressing up '^' key
         this.keyIn.up.on('down', function() {
             if(this.body.blocked.down) {
                 this.body.setVelocityY(-360);
             }
         }, this);
 
+        //space bar for something??????
+        //from prev iteration could recycle for something lul
         this.SPACE = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update(time, delta)
     {
+        //if player has boost
+        //add tween and sound
         if(this.hasBoost)
         {
             this.boost = 3;
+
+            this.add.tweens({
+                
+            });
         }
 
+        //general player movements [subject to change]
         if(this.keyIn.left.isDown) {
             this.body.setVelocityX(-200);
             this.x -= 2 * this.boost;
