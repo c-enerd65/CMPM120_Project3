16696a0e5566387cd09d694d21c26acada71e097
Sai <156489544+walinblue@users.noreply.github.com>
2025-11-16 20:22:00 -0800

    contextual files for map

---
+++ Tiles_2/monochrome_tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="monochrome_tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="400" columns="20">
+ <image source="../../../Downloads/kenney_1-bit-platformer-pack/Tilemap/monochrome_tilemap.png" width="339" height="339"/>
+</tileset>
