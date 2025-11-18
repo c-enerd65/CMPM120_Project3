f5e2307f91b052751a50f6125428ee21cb6f996f
Sai <saahamid@ucsc.edu>
2025-11-17 23:37:13 -0800

    added level two script

---
+++ asgn3_group/platformer/src/scenes/LevelTwo.js
@@ -0,0 +1,186 @@
+import Player from '../gameObjects/player.js';
+import Boost from '../gameObjects/boost.js';
+import Foe from '../gameObjects/foe.js';
+
+export class LevelTwo extends Phaser.Scene{
+    constructor() {
+        super('LevelTwo');
+    }  
+
+    create() {
+        //gets total screen width
+        this.width = this.sys.game.config.width;
+        this.center_w = this.width / 2;
+
+        //gets total screen height
+        this.height = this.sys.game.config.height;
+        this.center_h = this.height / 2;
+
+        //add tilemap
+        this.map = this.add.tilemap('tilemap_2');
+        var tileset = this.map.addTilesetImage('monochromeTilemap', 'monoTiles');
+        
+        //creates a new player, sets sprite scale 2x original size
+        this.player = new Player(this, 0, 250);
+
+        this.initParticles();
+        this.runTrail.start();
+
+        this.generateBoosts();
+        this.generateMobs();
+
+        this.mapCollisions(tileset);
+        this.levelCamera();
+        this.loadAudio();
+
+        this.playAudio('theme');
+
+        this.R = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+
+        this.levelCollisions();
+    }
+
+    update() {
+        this.player.update();
+        
+        /*if(this.player.lives <= 0)
+        {
+            this.player.destroy();
+
+            this.scene.stop(this.scene);
+            this.scene.start('End');
+        }*/
+
+        //remove later
+        if(Phaser.Input.Keyboard.JustDown(this.R)) {
+            this.resetGame();
+        }
+    }
+
+    mapCollisions(tileset) {
+        var ground = this.map.createLayer("ground", tileset, 0, 0);
+        ground.setCollisionBetween(1, this.width);
+        this.physics.add.collider(ground, this.player);
+        this.physics.add.collider(ground, this.foe);
+
+        var grab = this.map.createLayer("grab", tileset, 0, 0);
+        grab.setCollisionBetween(1, this.width);
+        this.physics.add.collider(grab, this.player);
+    }
+
+    levelCamera() {
+        //playing around with the camera settings [subject to change]
+        this.playerCam = this.cameras.main.setBounds(0, 0, this.width, this.height); //creates camera var
+        this.playerCam.startFollow(this.player, true, 0.5, 0.5, -200, 120); //sets camera to follow player
+        this.playerCam.setZoom(1.75, 1.75); //zooms the camera in
+    }
+
+    loadAudio() {
+        this.audioFiles = {
+            jump: this.sound.add('jump'),
+            theme: this.sound.add('lvl1_theme')
+        };
+    }
+
+    playAudio(key) {
+        this.audioFiles[key].play();
+    }
+
+    initParticles() {
+        this.runTrail = this.add.particles(this.player.x, this.player.y, 'star_1', {
+            quantity: 100,
+            accelerationX: 1000,
+            speedY: {
+                min: 20,
+                max: 20
+            },
+            speedX: {
+                min: 30,
+                max: 70
+            },
+            scale: {
+                start: 0.1,
+                end: 0.01,
+                random: true
+            },
+            blendMode: 'ADD',
+            frequency: -1,
+            follow: this.player,
+            followOffest: {},
+            tint: 0xffadd2 
+        });
+    }
+
+    generateBoosts() {
+        this.availBoost = this.add.group();
+
+        const bSpawn = {
+            x: [50, 150],
+            y: [350, 310]
+        }
+
+        for(let i = 0; i < bSpawn.x.length; i++) {
+            let rand = Math.ceil(Math.random() * 2)
+            let boost = new Boost(this, bSpawn.x[i], bSpawn.y[i], `boost_${rand}`);
+
+            this.availBoost.add(boost);
+        }
+    }
+
+    generateMobs() {
+        const foePath = () => {
+            this.path = new Phaser.Curves.Path(550, 400);
+            this.path.lineTo(650, 400);
+            this.graphics = this.add.graphics();
+        }
+
+        foePath()
+        this.path.draw(this.graphics);
+
+        this.foe = new Foe(this, this.path, 550, 340);
+    }
+
+    levelCollisions() {
+        this.physics.add.collider(
+            this.availBoost,
+            this.player,
+            this.playerBoost,
+            () => {
+                return true;
+            },
+            this
+        );
+
+    }
+
+    playerBoost(boost) {
+        //this.sound.play();
+
+        switch(boost.type) {
+            case 'stamina':
+                this.player.stamina += boost.modifier;
+                break;
+            case 'speed':
+                this.player.boost = boost.modifier;
+                this.runTrail.start();
+                this.tweens.add({
+                    targets: [this.player],
+                    completeDelay: 1500, // duration flexible
+                    onComplete: () => {
+                        this.player.boost = 1;
+                        this.runTrail.stop();
+                    }                     
+                });
+                break;
+        }
+
+        boost.destroy();
+    }
+
+    resetGame() {
+        this.player.destroy();
+
+        this.scene.stop(this.scene);
+        this.scene.start('LevelTwo');
+    }
+}
